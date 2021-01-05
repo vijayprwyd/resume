e6876f333558fc08218b2be9dacd165cccccb5ba
vijayprwyd <vijayprwyd@gmail.com>
2021-01-05 11:12:57 +0530

    Updates

---
+++ precache-manifest.6692c2789156b2720a89e684b71beb07.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dbdbb56097495712fdd3c8bc4499b04",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "db106af34b1858a9ef93",
+    "url": "/resume/static/css/main.337a8950.chunk.css"
+  },
+  {
+    "revision": "859f793dc1198263f06d",
+    "url": "/resume/static/js/2.ba47526f.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/resume/static/js/2.ba47526f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db106af34b1858a9ef93",
+    "url": "/resume/static/js/main.75af6fc0.chunk.js"
+  },
+  {
+    "revision": "8c1d914a1677cea53979",
+    "url": "/resume/static/js/runtime-main.75d8c91e.js"
+  },
+  {
+    "revision": "0ab7c70da56685277aa68643bdbc2587",
+    "url": "/resume/static/media/github.0ab7c70d.png"
+  },
+  {
+    "revision": "4cd7f43818621e24eaedaa3d9c3aa675",
+    "url": "/resume/static/media/hackerearth.4cd7f438.png"
+  },
+  {
+    "revision": "15fd5adb3c750e71a0e6ba69caaabe5f",
+    "url": "/resume/static/media/linkedin.15fd5adb.png"
+  },
+  {
+    "revision": "5233a552c9ee105ed4533a4081c29f79",
+    "url": "/resume/static/media/stack_overflow.5233a552.png"
+  }
+]);
