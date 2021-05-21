c49c3efd7663dfa41fc98d7e3b0a1719cf69bef4
Vijay P R <vijayprwyd@gmail.com>
2021-05-21 17:42:44 +0530

    Updated resume

--- src/WorkExperience/WorkExperience.js
+++ src/WorkExperience/WorkExperience.js
@@ -24,38 +24,40 @@ export function WorkExperience() {
               Configuring the billing plugins for different environments ( prod
               / dev ), setup best practices for repositories using development
               tools like prettier, es-lint, git hooks to prevent bad commits ,
-              set up C/CD pipeline in Jenkins to prevent merging bad PRs{" "}
+              set up CI/CD pipeline in Jenkins to prevent merging bad PRs.{" "}
             </li>
             <li>
               Regularly updating documentation of our repositories, upgrading
               libraries, participate in design discussions, doing code reviews,
-              controlling the branching strategy  for weekly releases, deploying
-              builds to production and monitoring post deployment
+              controlling the branching strategy for weekly releases, deploying
+              builds to production and monitoring post deployment.
             </li>
-            <li>Integrating component libraries with storybooks</li>
+            <li>Integrating component libraries with storybooks.</li>
             <li>
               Interact with PMs, XDs and backend engineers for understanding the
               requirements, feature estimations, designing and developing pages
-              for various subscription related flows ( Subscription setup,
-              Upgrade, Downgrade, switching between monthly and annual plans,
-              cancel, resubscribe, add-on subscriptions etc)
+              for various subscription related flows ( subscription setup,
+              upgrade, downgrade, switching between monthly and annual plans,
+              cancel, resubscribe, add-on subscriptions etc).
             </li>
             <li>
               Follow a test driven development approach, ensuring proper unit
               test coverage, and e2e tests with Cypress ensuring zero manual
-              testing effort
+              testing effort.
             </li>
             <li>
               Ensure that the page is responsive, passes lighthouse tests for
-              performance and accessibility and certified by security plugins{" "}
+              performance and accessibility and certified by security plugins.{" "}
             </li>
             <li>
-              Take care of internationalization of contents in various locales
+              Take care of internationalization of contents in various locales.
             </li>
             <li>
               Developed framework for studying user interactions and logging the
               events in Amplitude, which was required for various AI driven
-              frameworks and marketing team
+              frameworks and marketing team. Creating dashboards and developing
+              charts with libraries like Highcharts for capruting various
+              business metrics.
             </li>
             <li>
               Implemented loggers to log errors to Splunk, create alerts and
@@ -66,7 +68,7 @@ export function WorkExperience() {
             <li>
               Analysed performance issues and improving it with techniques like
               code splitting, lazy loading, caching, parallelization of network
-              calls, preloading and pre-fetching. Brought down page loads from
+              calls, preloading, pre-fetching etc. Brought down page loads from
               10 sec to less than 4 sec
             </li>
           </ul>
@@ -80,8 +82,9 @@ export function WorkExperience() {
             <li>
               {" "}
               Converted synchronous subscription creation flows to asynchronous
-              to improve performance ( Spring Boot, JMS , AWS Aurora ){" "}, enhancing and developing  Restful APIs to support new product
-              requirements
+              to improve performance ( Spring Boot, JMS , AWS Aurora ) ,
+              enhancing and developing Restful APIs to support new product
+              requirements.
             </li>
           </ul>
         </div>
